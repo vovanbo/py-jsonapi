ecaa32b17a96366748932cd5911c1f91f518812d
Benedikt Schmitt <benedikt@benediktschmitt.de>
2016-03-13 19:43:07 +0100

    *reduced* coupling by implementing a new classmethod *from_request*

--- jsonapi/base/pagination.py
+++ jsonapi/base/pagination.py
@@ -32,8 +32,7 @@ http://jsonapi.org/format/#fetching-pagination
 
 # std
 from collections import OrderedDict
-import math
-import urllib
+import urllib.parse
 
 # third party
 from cached_property import cached_property
@@ -43,59 +42,74 @@ class Pagination(object):
     """
     A helper class for the pagination.
 
-    The fist page has the number *1*.
+    The fist page has the number *0*.
 
-    :arg jsonapi.base.request.Request request:
-        The current jsonapi request
+    :arg str uri:
+        The uri to the endpoint which provides a collection
+    :arg int current_page:
+        The number of the current page
+    :arg int page_size:
+        The number of resources on a page
     :arg int total_resources:
-        The total number of resources, which would have been returned without
-        the pagination.
-
-    .. seealso::
-
-        *   :attr:`jsonapi.base.request.Request.japi_page_size`
-        *   :attr:`jsonapi.base.request.Request.japi_page_number`
-        *   :attr:`jsonapi.base.request.Request.japi_paginate`
-        *   http://jsonapi.org/format/#fetching-pagination
+        The total number of resources in the collection
     """
 
-    def __init__(self, request, total_resources):
+    def __init__(self, uri, current_page, page_size, total_resources):
         """
         """
-        assert request.japi_paginate
-
-        self.request = request
+        self.uri = uri
+        self.parsed_uri = urllib.parse.urlparse(uri)
+        self.query_uri = urllib.parse.parse_qs(self.parsed_uri.query)
 
         # Get the current page number and size.
-        self.current_page = self.request.japi_page_number
-        self.page_size = self.request.japi_page_size
+        assert current_page >= 0
+        self.current_page = current_page
+
+        assert page_size >= 1
+        self.page_size = page_size
 
         # Get the number of resources
+        assert total_resources >= 0
         self.total_resources = total_resources
-        self.total_pages = math.ceil(self.total_resources/self.page_size)
+
+        self.total_pages = int(self.total_resources/self.page_size)
 
         # Build all links
         self.link_self = self._page_link(self.current_page, self.page_size)
-        self.link_first = self._page_link(1, self.page_size)
+        self.link_first = self._page_link(0, self.page_size)
         self.link_last = self._page_link(self.total_pages, self.page_size)
 
-        self.has_prev = (self.current_page > 1)
+        self.has_prev = (self.current_page > 0)
         self.link_prev = self._page_link(self.current_page - 1, self.page_size)
 
         self.has_next = (self.current_page < self.total_pages)
         self.link_next = self._page_link(self.current_page + 1, self.page_size)
         return None
 
+    @classmethod
+    def from_request(cls, request, total_resources):
+        """
+        Shortcut for creating a Pagination object based on a jsonapi
+        :class:`Request`.
+        """
+        assert request.japi_paginate
+        return cls(
+            uri=request.uri, current_page=request.japi_page_number,
+            page_size=request.japi_page_size, total_resources=total_resources
+        )
+
     def _page_link(self, page_number, page_size):
-        parsed_uri = self.request.parsed_uri
-        query = urllib.parse.urlencode({
+        query = self.query_uri
+        query.update({
             "page[number]": page_number,
             "page[size]": page_size
         })
+        query = urllib.parse.urlencode(query)
+
         uri = "{scheme}://{netloc}{path}?{query}".format(
-            scheme=parsed_uri.scheme,
-            netloc=parsed_uri.netloc,
-            path=parsed_uri.path,
+            scheme=self.parsed_uri.scheme,
+            netloc=self.parsed_uri.netloc,
+            path=self.parsed_uri.path,
             query=query
         )
         return uri
@@ -103,7 +117,20 @@ class Pagination(object):
     @cached_property
     def json_meta(self):
         """
-        Must be included in the top-level meta object.
+        A dictionary, which must be included in the top-level meta object. It
+        contains these keys:
+
+        *   *total-pages*
+            The total number of pages
+
+        *   *total-resources*
+            The total number of resources
+
+        *   *page*
+            The number of the current page
+
+        *   *page-size*
+            The page size
         """
         d = OrderedDict()
         d["total-pages"] = self.total_pages
@@ -115,7 +142,23 @@ class Pagination(object):
     @cached_property
     def json_links(self):
         """
-        Must be included in the top-level links object.
+        A dictionary, which must be included in the top-level links object. It
+        contains these keys:
+
+        *   *self*
+            The link to the current page
+
+        *   *first*
+            The link to the first page
+
+        *   *last*
+            The link to the last page
+
+        *   *prev*
+            The link to the previous page (only set, if a previous page exists)
+
+        *   *next*
+            The link to the next page (only set, if a next page exists)
         """
         d = OrderedDict()
         d["self"] = self.link_self
